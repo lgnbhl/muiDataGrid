608f6625a57f217dbc41d440f2dbadca16a0ecee
lgnbhl <felix.luginbuhl@protonmail.ch>
2026-06-12 10:52:14 +0200

    add MUI X Data Grid locales for multi languages

--- js/src/index.js
+++ js/src/index.js
@@ -1,5 +1,6 @@
 window.jsmodule = {
   ...window.jsmodule,
   '@mui/x-data-grid': require('@mui/x-data-grid'),
+  '@mui/x-data-grid/locales': require('@mui/x-data-grid/locales'),
   '@muiDataGrid/custom': require('./ServerSideDataGrid'),
 };
